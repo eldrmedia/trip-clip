79d0b5a75079eae38af9e1ac0d20670902364840
Will Elder <will@eldrmedia.com>
2025-09-21 17:23:25 -0500

    feat: adds vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import Header from "@/components/Header";
 import { Toaster } from "react-hot-toast";
 
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className="bg-gray-50 min-h-screen">
         <Header />
         <main className="px-4 py-6">{children}</main>
